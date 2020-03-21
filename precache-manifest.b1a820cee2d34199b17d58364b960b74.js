0403f35efe6e6ca94c6701283236e2606f9ba8af
Tiago Rossi <34634082+tiagorossig@users.noreply.github.com>
2020-03-21 15:47:58 -0500

    Updates

---
+++ precache-manifest.b1a820cee2d34199b17d58364b960b74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed350fd939298b1b0ff6fbba7cd82a05",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "348a83e4c8833a6e8017",
+    "url": "/personal-website/static/css/main.b312a01f.chunk.css"
+  },
+  {
+    "revision": "5512666c3da13cbdb81c",
+    "url": "/personal-website/static/js/2.f607e39f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal-website/static/js/2.f607e39f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "348a83e4c8833a6e8017",
+    "url": "/personal-website/static/js/main.12d13181.chunk.js"
+  },
+  {
+    "revision": "3ed81d5cbe34b8280bac",
+    "url": "/personal-website/static/js/runtime-main.442aafae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
