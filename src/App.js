e21ddc3c6809633c2f9fff306948e5e7c0ecc186
Tiago Rossi <34634082+tiagorossig@users.noreply.github.com>
2020-03-24 02:19:00 -0500

    added home page template

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from "react";
 import "./App.css";
+import Home from "./components/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>My React App</h1>
-      <h2>Author: Tiago Grimaldi Rossi</h2>
-      <h3>website coming soon...</h3>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Home(props) {
+  return (
+    <header class="masthead">
+      <div class="container h-100">
+        <div class="row h-100 align-items-center">
+          <div class="col-12 text-center">
+            <h1 class="font-weight-light">
+              Vertically Centered Masthead Content
+            </h1>
+            <p class="lead">A great starter layout for a landing page</p>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Home;
