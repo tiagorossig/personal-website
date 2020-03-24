c46befd5a0dbcf975b4093fbe6ace7a95fe75622
Tiago Rossi <34634082+tiagorossig@users.noreply.github.com>
2020-03-24 18:05:25 -0500

    formatting

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,53 +6,55 @@ import { faCode } from "@fortawesome/free-solid-svg-icons";
 function Portfolio(props) {
   return (
     <React-Fragment>
-      <div class="portfolio">
-        {/* Page Heading */}
-        <h1 class="my-4">Projects</h1>
+      <div class="center">
+        <div class="portfolio">
+          {/* Page Heading */}
+          <h1 class="my-4">Projects</h1>
 
-        <hr />
+          <hr />
 
-        {/* Project Two */}
-        <div class="row">
-          <div class="col-md-5">
-            <a href="/">
-              <img
-                class="img-fluid rounded mb-3 mb-md-0"
-                src="http://placehold.it/700x300"
-                alt=""
-              />
-            </a>
+          {/* Project Two */}
+          <div class="row">
+            <div class="col-md-5">
+              <a href="/">
+                <img
+                  class="img-fluid rounded mb-3 mb-md-0"
+                  src="http://placehold.it/350x350"
+                  alt=""
+                />
+              </a>
+            </div>
+            <div class="col-md-5">
+              <h3>Project Two</h3>
+              <p>Project 2.</p>
+              <a class="btn btn-dark" href="/">
+                <FontAwesomeIcon icon={faCode} />
+                <b> Code</b>
+              </a>
+            </div>
           </div>
-          <div class="col-md-5">
-            <h3>Project Two</h3>
-            <p>Project 2.</p>
-            <a class="btn btn-dark" href="/">
-              <FontAwesomeIcon icon={faCode} />
-              <b> Code</b>
-            </a>
-          </div>
-        </div>
 
-        <hr />
+          <hr />
 
-        {/* Project Three */}
-        <div class="row">
-          <div class="col-md-5">
-            <a href="/">
-              <img
-                class="img-fluid rounded mb-3 mb-md-0"
-                src="http://placehold.it/700x300"
-                alt=""
-              />
-            </a>
-          </div>
-          <div class="col-md-5">
-            <h3>Project Three</h3>
-            <p>Project 3.</p>
-            <a class="btn btn-dark" href="/">
-              <FontAwesomeIcon icon={faCode} />
-              <b> Code</b>
-            </a>
+          {/* Project Three */}
+          <div class="row">
+            <div class="col-md-5">
+              <a href="/">
+                <img
+                  class="img-fluid rounded mb-3 mb-md-0"
+                  src="http://placehold.it/350x350"
+                  alt=""
+                />
+              </a>
+            </div>
+            <div class="col-md-5">
+              <h3>Project Three</h3>
+              <p>Project 3.</p>
+              <a class="btn btn-dark" href="/">
+                <FontAwesomeIcon icon={faCode} />
+                <b> Code</b>
+              </a>
+            </div>
           </div>
         </div>
       </div>
