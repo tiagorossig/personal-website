2538cd848f01ce7c3314385b5ff0e97983845c04
Tiago Rossi <34634082+tiagorossig@users.noreply.github.com>
2020-03-25 15:50:23 -0500

    added background pictures back, text is transparent at start

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -11,72 +11,58 @@ function Portfolio(props) {
     <header className="masthead">
       <div className="center" id="padding-notHome">
         <div className="row">
-          <div className="col-lg-6 mb-4" id="tester">
-            <div className="project-container">
-              <p className="project-title">Spectrum Internship</p>
+          <div className="col-lg-6 project-container" id="tester">
+            <p className="project-title">Spectrum Internship</p>
+            <a href="/">
+              <div className="overlay"></div>
+            </a>
+          </div>
+
+          <div className="col-lg-6 project-container" id="project-one">
+            <p className="project-title">Cloud Care</p>
+            <a href="/">
               <div className="overlay"></div>
               <div className="project-button">
-                <a className="btn btn-dark" href="/">
+                <a className="btn btn-dark disabled" href="/">
                   <FontAwesomeIcon icon={faCode} />
                   <b className="button-text"> Code</b>
                 </a>
               </div>
-            </div>
-          </div>
-
-          <div className="col-lg-6 mb-4" id="tester">
-            <div className="project-container" id="project-one">
-              <a href="/">
-                <p className="project-title">Cloud Care</p>
-                <div className="overlay"></div>
-                <div className="project-button">
-                  <a className="btn btn-dark disabled" href="/">
-                    <FontAwesomeIcon icon={faCode} />
-                    <b className="button-text"> Code</b>
-                  </a>
-                </div>
-              </a>
-            </div>
+            </a>
           </div>
         </div>
 
         <div className="row">
-          <div className="col-lg-6 mb-4" id="tester">
-            <div className="project-container">
-              <a href="https://tiagoros.itch.io/degraded">
-                <img className="card-img-top project-img" src="" alt="" />
-                <p className="project-title">de;graded</p>
-                <div className="overlay"></div>
-                <div className="project-button">
-                  <a
-                    className="btn btn-dark"
-                    href="https://github.com/tiagorossig/repairRobot-"
-                  >
-                    <FontAwesomeIcon icon={faCode} />
-                    <b className="button-text"> Code</b>
-                  </a>
-                </div>
-              </a>
-            </div>
+          <div className="col-lg-6 project-container" id="project-two">
+            <p className="project-title">de;graded</p>
+            <a href="https://tiagoros.itch.io/degraded">
+              <div className="overlay"></div>
+              <div className="project-button">
+                <a
+                  className="btn btn-dark"
+                  href="https://github.com/tiagorossig/repairRobot-"
+                >
+                  <FontAwesomeIcon icon={faCode} />
+                  <b className="button-text"> Code</b>
+                </a>
+              </div>
+            </a>
           </div>
 
-          <div className="col-lg-6 mb-4" id="tester">
-            <div className="project-container">
-              <a href="https://pingwoh.itch.io/fledgeling">
-                <img className="card-img-top project-img" src="" alt="" />
-                <p className="project-title">Fledgeling</p>
-                <div className="overlay"></div>
-                <div className="project-button">
-                  <a
-                    className="btn btn-dark"
-                    href="https://github.com/tiagorossig/flyingHome"
-                  >
-                    <FontAwesomeIcon icon={faCode} />
-                    <b className="button-text"> Code</b>
-                  </a>
-                </div>
-              </a>
-            </div>
+          <div className="col-lg-6 project-container" id="project-three">
+            <p className="project-title">Fledgeling</p>
+            <a href="https://pingwoh.itch.io/fledgeling">
+              <div className="overlay"></div>
+              <div className="project-button">
+                <a
+                  className="btn btn-dark"
+                  href="https://github.com/tiagorossig/flyingHome"
+                >
+                  <FontAwesomeIcon icon={faCode} />
+                  <b className="button-text"> Code</b>
+                </a>
+              </div>
+            </a>
           </div>
         </div>
       </div>
