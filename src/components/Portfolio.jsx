598e74e86ea3f71a2ad7cc4416c5a6c7929289a6
Tiago Rossi <34634082+tiagorossig@users.noreply.github.com>
2020-03-25 01:08:08 -0500

    project images

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "../App.css";
+import projectOne from "../assets/cloudCare.png";
+import projectThree from "../assets/fledge.png";
 
 function Home(props) {
   return (
@@ -8,12 +10,8 @@ function Home(props) {
         <div className="row">
           <div className="col-lg-6 mb-4">
             <div class="project-container">
-              <img
-                className="card-img-top project-img"
-                src="http://placehold.it/700x400"
-                alt=""
-              />
-              <p class="project-title">card title</p>
+              <img className="card-img-top project-img" src="" alt="" />
+              <p class="project-title">Spectrum Internship</p>
               <div class="overlay"></div>
               <div class="project-button">
                 <a href="/"> BUTTON </a>
@@ -25,10 +23,10 @@ function Home(props) {
             <div class="project-container">
               <img
                 className="card-img-top project-img"
-                src="http://placehold.it/700x400"
+                src={projectOne}
                 alt=""
               />
-              <p class="project-title">card title</p>
+              <p class="project-title">Cloud Care</p>
               <div class="overlay"></div>
               <div class="project-button">
                 <a href="/"> BUTTON </a>
@@ -57,10 +55,10 @@ function Home(props) {
             <div class="project-container">
               <img
                 className="card-img-top project-img"
-                src="http://placehold.it/700x400"
+                src={projectThree}
                 alt=""
               />
-              <p class="project-title">card title</p>
+              <p class="project-title">Fledgeling</p>
               <div class="overlay"></div>
               <div class="project-button">
                 <a href="/"> BUTTON </a>
