dc23b9d1b60e1375461babd9acc2160164f26af3
Tiago Rossi <34634082+tiagorossig@users.noreply.github.com>
2020-03-24 23:52:16 -0500

    footer fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Footer(props) {
   return (
-    <footer id="sticky-footer" class="py-4 bg-dark text-white-50">
+    <footer id="footer" class="py-4 bg-dark text-white-50">
       <div class="container text-center">
-        <small>&copy; Tiago Grimaldi Rossi 2020</small>
+        <small>Copyright &copy; Your Website</small>
       </div>
     </footer>
   );
