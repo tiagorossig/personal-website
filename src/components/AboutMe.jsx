d15a9c5440e22d2f05a3b442ffa1241666b55f4c
Tiago Rossi <34634082+tiagorossig@users.noreply.github.com>
2020-08-30 13:11:10 -0500

    finished AboutMe design, missing the text

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,53 +6,15 @@ import tiago from "../assets/tiago.jpg";
 
 function AboutMe(props) {
   return (
-    <header className="masthead" id="childScroll">
-      {/* <div className="tempTest"></div> */}
-      <div className="tableContainer1">
-        <div className="tableContainer2">
-          <table className="test" id="table_id">
-            <tr>
-              <td className="firstHalf">
-                <div className="frame">
-                  <img src={tiago} className="oval" />
-                </div>
-              </td>
-              <td className="secondHalf">
-                <div className="aboutText ">
-                  <h1> Who Am I? </h1>
-                  <p>
-                    {" "}
-                    Hi! I'm Adrian, a junior Computer Science student at the
-                    University of Texas at Austin. I was born in Cardiff, Wales,
-                    though I've spent most of my life in Detroit, Michigan and
-                    Mexico City. I play classical piano, enjoy reading and
-                    writing, watching Real Madrid games, and I never miss a
-                    Formula 1 race.{" "}
-                  </p>
-                  <p>
-                    {" "}
-                    My Computer Science coursework, side projects and work
-                    experience descriptions can be found to the right. I'm
-                    currently seeking a software engineering internship for the
-                    following summer/winter, and if you're hiring, please don't
-                    hesitate to contact me at the email address below.{" "}
-                  </p>
-                </div>
-              </td>
-            </tr>
-          </table>
+    <>
+      <div className="row align-items-center padding-navBar">
+        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-6 align-self-center padding-tiago">
+          <img src={tiago} className="oval" />
         </div>
-      </div>
 
-      {/* <tr border="5">
-        <td>
-          <div className="frame">
-            <img src={tiago} className="oval" />
-          </div>
-        </td>
-        <td bgcolor="orange">
-          <div className="aboutText ">
-            <h1> Who Am I? </h1>
+        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4">
+          <div className="aboutText">
+            <h1 className="heading"> Who Am I? </h1>
             <p>
               {" "}
               Hi! I'm Adrian, a junior Computer Science student at the
@@ -70,9 +32,9 @@ function AboutMe(props) {
               email address below.{" "}
             </p>
           </div>
-        </td>
-      </tr> */}
-    </header>
+        </div>
+      </div>
+    </>
   );
 }
 
