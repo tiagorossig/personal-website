5cc211d3a858a7622f024aaa937f6c5316afa5dd
Tiago Rossi <34634082+tiagorossig@users.noreply.github.com>
2020-08-31 11:19:57 -0500

    updated who am I text

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import "../App.css";
 import tiago from "../assets/tiago.jpg";
 
-// col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12 text-center
-
 function AboutMe(props) {
   return (
     <>
@@ -16,20 +14,26 @@ function AboutMe(props) {
           <div className="aboutText">
             <h1 className="heading"> Who Am I? </h1>
             <p>
-              {" "}
-              Hi! I'm Adrian, a junior Computer Science student at the
-              University of Texas at Austin. I was born in Cardiff, Wales,
-              though I've spent most of my life in Detroit, Michigan and Mexico
-              City. I play classical piano, enjoy reading and writing, watching
-              Real Madrid games, and I never miss a Formula 1 race.{" "}
+              Hey! I’m Tiago, a third-year Computer Science student at the
+              University of Texas at Austin. I lived the first fifteen years of
+              my life in São Paulo, Brazil—I absolutely loved it there. I moved
+              with my family to Austin, Texas, where I started my career as a
+              software developer. I’ve experimented with web development and
+              built games and mobile apps. This past summer I had a great time
+              interning at Spectrum Enterprise, and I’m looking forward to my
+              next opportunity. Throughout my career, I hope to solve
+              interesting problems with scalable and elegant solutions.
             </p>
             <p>
-              {" "}
-              My Computer Science coursework, side projects and work experience
-              descriptions can be found to the right. I'm currently seeking a
-              software engineering internship for the following summer/winter,
-              and if you're hiring, please don't hesitate to contact me at the
-              email address below.{" "}
+              Software development aside, I enjoy weight lifting, doing
+              calisthenics, playing PC video games (League of Legends for the
+              most part), and geeking out about the latest computer part or
+              piece of tech that is coming out.
+            </p>
+            <p>
+              Anyways, I’m currently seeking a software engineering internship
+              for Summer and Fall 2021. If you’re hiring, feel free to contact
+              me through email or Linkedin (see my Home page).
             </p>
           </div>
         </div>
